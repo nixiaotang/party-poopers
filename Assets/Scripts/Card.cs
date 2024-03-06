23b269af70de84176d8e99ae895fa0cc137e0883
Lily Ni <nixiaotang2004@gmail.com>
2024-03-05 22:32:54 -0500

    basic card logic

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public enum Type
     {
@@ -14,23 +16,44 @@ public class Card : MonoBehaviour
         Dark
     }
 
+
     [System.Serializable]
     public class CardInfo
     {
         public string name;
         public Type type;
-        public int cost;
         public int damage;
+        public int cost;
+        public string description;
     }
 
+    [SerializeField] private CardInfo _card;
+    [SerializeField] private TextMeshProUGUI _nameText;
+    [SerializeField] private TextMeshProUGUI _damageText;
+    [SerializeField] private TextMeshProUGUI _costText;
+    [SerializeField] private TextMeshProUGUI _descText;
 
-    [SerializeField] private CardInfo _cardInfo;
+    private CardManager _cardManager;
 
-
-    void Start()
+    public void Init(CardInfo cardInfo)
     {
-        
+        _card = cardInfo;
+        _cardManager = transform.parent.GetComponent<CardManager>();
 
+        _nameText.text = _card.name;
+        transform.name = _card.name + " - Card";
+        _damageText.text = $"Damage: {_card.damage}";
+        _costText.text = $"Cost: {_card.cost}";
+        _descText.text = _card.description;
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+    }
 }
---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    [SerializeField] private List<Card.CardInfo> cardInfos;
+    [SerializeField] private GameObject cardPrefab;
+
+    private List<Card> cards = new();
+
+    private void Start()
+    {
+        // instantiate every card specified by cardInfos list
+        for(int i = 0; i < cardInfos.Count; i++)
+        {
+            GameObject cardObj = Instantiate(cardPrefab, this.transform, false);
+            cardObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(-((cardInfos.Count-1) * 110 / 2) + i * 110, 140);
+
+            Card card = cardObj.GetComponent<Card>();
+            card.Init(cardInfos[i]);
+            cards.Add(card);
+        }
+    }
+
+}
