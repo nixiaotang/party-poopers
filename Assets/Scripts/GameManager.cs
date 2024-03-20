79ce8f984332675f100c1ca6ac755677ea44f9cc
vrnprkh <paintingburritos@gmail.com>
2024-03-20 19:18:44 -0400

    garbage ui

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private int numPartyMembers = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numPartyMembers; i++)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
