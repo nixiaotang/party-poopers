ce217c623bdaa4a7033d5f78dc359cbdb9d7f0bd
vrnprkh <paintingburritos@gmail.com>
2024-03-21 17:19:45 -0400

    Unit hand management

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,6 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private int numPartyMembers = 1;
 
-
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Unit : MonoBehaviour
+{
+    [SerializeField]
+    private int health = 10;
+
+    [SerializeField]
+    private int baseMana = 10;
+
+    // NOTES:
+    //  deck is also just used as draw
+    //  by convention, end of deck is TOP
+    //  front of deck is BOTTOM
+          
+    [SerializeField]
+    private List<Card> deck;
+
+    private List<Card> hand;
+    private List<Card> discard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shuffle();
+    }
+
+    
+    // moves discard to deck, then shuffles
+    void Shuffle()
+    {
+        deck.AddRange(discard);
+        discard.Clear();
+
+        // todo: check this lol
+        var rng = new System.Random();
+        deck = deck.OrderBy(_ => rng.Next()).ToList();
+    }
+
+    public void DrawCards(int n = 1)
+    {
+        for (int i = 0; i < n; i ++)
+        {
+            if (deck.Count == 0)
+            {
+                Shuffle();
+                // if no more cards to draw stop
+                if (deck.Count == 0)
+                {
+                    break;
+                }
+            }
+            hand.Add(deck[deck.Count - 1]);
+            deck.RemoveAt(deck.Count - 1);
+        }
+    }
+
+    // get hand
+    public List<Card> getHand()
+    {
+        return hand;
+    }
+
+    // moves a card in a spot to discard, and then return it
+    // we might not need to return the card
+    public Card moveToDiscard(int n)
+    {
+        if (n >= hand.Count)
+        {
+            throw new System.ArgumentException(string.Format("Invalid card number: {0} max is: {1}", n, hand.Count), "n");
+        }
+        var playedCard = hand[n];
+        hand.RemoveAt(n);
+        return playedCard;
+    }
+
+}
